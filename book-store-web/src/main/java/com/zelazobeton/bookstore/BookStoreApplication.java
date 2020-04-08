be5c80b77a33749dc6b6d52ee62e82a5674627f4
Franciszek Bubnicki <franciszek.bubnicki@gmail.com>
2020-04-08 21:24:10 +0200

    pom fix

--- book-store-web/src/main/java/com/zelazobeton/bookstore/BookStoreApplication.java
+++ book-store-web/src/main/java/com/zelazobeton/bookstore/BookStoreApplication.java
@@ -2,9 +2,10 @@ package com.zelazobeton.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
